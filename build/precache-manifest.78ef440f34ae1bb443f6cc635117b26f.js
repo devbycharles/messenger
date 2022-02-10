810f3a4768789b405ea6536919cc719c414a8102
Charles Rocha <95267825+devbycharles@users.noreply.github.com>
2022-02-09 21:17:55 -0800

    Add files via upload
    Initial Commit

---
+++ build/precache-manifest.78ef440f34ae1bb443f6cc635117b26f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc939c3104a22d9643a1d872a2082e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82ca5d898a75da956ebe",
+    "url": "/static/css/main.be596253.chunk.css"
+  },
+  {
+    "revision": "170c609e793186b7c0f6",
+    "url": "/static/js/2.6cbf288a.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/static/js/2.6cbf288a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82ca5d898a75da956ebe",
+    "url": "/static/js/main.80de9767.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
